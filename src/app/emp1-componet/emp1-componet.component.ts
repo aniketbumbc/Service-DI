fd05b28415ed9b641758744dc00cff7ab77053d0
aniket umbc <aniket.umbc@gmail.com>
2018-01-30 00:09:44 +0530

    Add dependancy

--- src/app/emp1-componet/emp1-componet.component.ts
+++ src/app/emp1-componet/emp1-componet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{EmployeeService}from "../employee.service";
 
 @Component({
   selector: 'app-emp1-componet',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Emp1ComponetComponent implements OnInit {
 employee=[];
-  constructor() { }
+  constructor(private  _empservice:EmployeeService) { }  // local variable give instances of employee services 
 
   ngOnInit() {
+
+    this.employee=this._empservice.getEmployee();
   }
 
 }
