b21b64e6bd25c8d6e76199131c0b006503d58d15
aniket umbc <aniket.umbc@gmail.com>
2018-01-30 08:20:53 +0530

    dependancy injection Done with css

--- src/app/emp1-componet/emp1-componet.component.ts
+++ src/app/emp1-componet/emp1-componet.component.ts
@@ -5,23 +5,23 @@ import { EmployeeService } from "../employee.service";
 @Component({
   selector: 'app-emp1-componet',
   template: `
+  <div class="abc">
   <h2>Employee List and name </h2>
-  <h3>{{errorMsg}}</h3>
   <ul *ngFor="let emplo of employee">
-    <li>
-    {{emplo.id}}
-     {{emplo.name}} 
- 
-     
-     </li>
+    <li>{{emplo.id}} {{emplo.name}} </li>
   </ul>
+  </div>
 `,
-  styles: []
+  styles:[`
+    .abc{
+    background: green;
+    color:red;
+  }`]
 
 })
 export class Emp1ComponetComponent implements OnInit {
   public employee = [];
-  public errorMsg;
+
   constructor(public _empservice: EmployeeService) { }  // local variable give instances of employee services 
   ngOnInit() {
 
