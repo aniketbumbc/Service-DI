6891a0fecfa58a1a50bc19678bcebfe21869b260
aniket umbc <aniket.umbc@gmail.com>
2018-01-30 08:44:50 +0530

    Added emp-info componet

---
+++ src/app/Iprofile.ts
@@ -0,0 +1,5 @@
+export interface Iporfile{
+    Name:string;
+    Age:number;
+    School:string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Emp1ComponetComponent } from './emp1-componet/emp1-componet.component';
 import { Emp2ComponetComponent } from './emp2-componet/emp2-componet.component';
+import { EmpInfoComponent } from './emp-info/emp-info.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Emp1ComponetComponent,
-    Emp2ComponetComponent
+    Emp2ComponetComponent,
+    EmpInfoComponent
   ],
   imports: [
     BrowserModule
